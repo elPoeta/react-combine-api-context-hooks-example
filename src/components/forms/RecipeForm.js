a94a56e5faaac118b9a2f2372dee946a1f770679
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-01-14 20:33:42 -0300

    recipe form

--- src/components/forms/RecipeForm.js
+++ src/components/forms/RecipeForm.js
@@ -1,13 +1,38 @@
-import React from 'react'
-
+import React, {useState, useContext} from 'react'
+import {RecipeContext} from '../../contexts/RecipeContext';
+import {ADD_RECIPE} from '../../actions/types';
 const RecipeForm = () => {
+  const { dispatch } = useContext(RecipeContext);
+  const [title, setTitle] = useState('');
+  const [cookTime, setCookTime] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [servings, setServings] = useState('');
+   
+  const handleSubmit = e =>{ 
+     e.preventDefault();
+     dispatch(
+       {
+         type:ADD_RECIPE, 
+         recipe: {
+           title,
+           ingredients,
+           cookTime,
+           servings
+         }
+        })
+        setTitle('');
+        setIngredients('');
+        setServings('');
+        setCookTime('');
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" name="title" placeholder="Title"  required />
-        <textarea rows="10" placeholder="Ingredients" required/>
-        <input type="number" name="cookTime" placeholder="Cook Time"  required />
-        <input type="number" name="title" placeholder="Servings"  required />
+      <form onSubmit={handleSubmit}> 
+        <input type="text" name="title" placeholder="Title"  value={title} onChange={e => setTitle(e.target.value)} required />
+        <textarea rows="10" placeholder="Ingredients" value={ingredients} onChange={e=> setIngredients(e.target.value)} required/>
+        <input type="number" name="cookTime" placeholder="Cook Time" value={cookTime}  onChange={e => setCookTime(e.target.value)} required />
+        <input type="number" name="servings" placeholder="Servings" value={servings} onChange={e=> setServings(e.target.value)} required />
         <input type="submit" value="add recipe" />
       </form>
     </div>
