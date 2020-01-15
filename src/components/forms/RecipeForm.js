9a1ddb0a64ee5454cf794ae83cd0ba829864f3f7
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-01-14 21:34:37 -0300

    style form

--- src/components/forms/RecipeForm.js
+++ src/components/forms/RecipeForm.js
@@ -1,6 +1,8 @@
 import React, {useState, useContext} from 'react'
 import {RecipeContext} from '../../contexts/RecipeContext';
 import {ADD_RECIPE} from '../../actions/types';
+import './recipeForm.css';
+
 const RecipeForm = () => {
   const { dispatch } = useContext(RecipeContext);
   const [title, setTitle] = useState('');
@@ -27,13 +29,14 @@ const RecipeForm = () => {
   }
 
   return (
-    <div>
+    <div className="RecipeForm">
+      <h2>Create Recipe</h2>
       <form onSubmit={handleSubmit}> 
-        <input type="text" name="title" placeholder="Title"  value={title} onChange={e => setTitle(e.target.value)} required />
-        <textarea rows="10" placeholder="Ingredients" value={ingredients} onChange={e=> setIngredients(e.target.value)} required/>
+        <input className="RecipeForm-full" type="text" name="title" placeholder="Title"  value={title} onChange={e => setTitle(e.target.value)} required />
+        <textarea className="RecipeForm-full" rows="10" placeholder="Ingredients" value={ingredients} onChange={e=> setIngredients(e.target.value)} required/>
         <input type="number" name="cookTime" placeholder="Cook Time" value={cookTime}  onChange={e => setCookTime(e.target.value)} required />
         <input type="number" name="servings" placeholder="Servings" value={servings} onChange={e=> setServings(e.target.value)} required />
-        <input type="submit" value="add recipe" />
+        <input className="RecipeForm-full RecipeForm-btnSubmit" type="submit" value="add recipe" />
       </form>
     </div>
   )
